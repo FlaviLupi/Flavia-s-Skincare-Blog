71afb87ca631b2e87b14ced8add655ed94d266ab
FlaviLupi <flaviallupu@gmail.com>
2025-04-15 17:56:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function openMain(){
+    window.location.href='index.html';
+}
+function openSkincareRoutine(){
+    window.location.href='skincareroutine.html';
+}
+function openHomemadeFaceMasks(){
+    window.location.href='homemadefacemasks.html';
+}
+function openTipsAndTricks(){
+    window.location.href='tipsandtricks.html';
+}
+function openReviews(){
+    window.location.href='reviews.html';
+}
